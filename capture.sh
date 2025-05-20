5e2c25676f3a28795480aab22cdab6b9df7cc639
Daniel Hancock <83016638+daniel-hancock33@users.noreply.github.com>
2025-05-20 17:01:38 -0500

    fixes issues

--- capture.sh
+++ capture.sh
@@ -3,18 +3,20 @@
 trap 'echo "💥 Ctrl+C received, killing all jobs..."; kill 0; exit 1' SIGINT
 
 CAPTUREID=$(date +%Y%m%d%H%M%S)
-BASE_PATH="/opt/ibm/appliance/storage/scratch/sysstat"
+BASE_PATH="/opt/ibm/appliance/storage/scratch/iias-sysstat"
 LIB_PATH="${BASE_PATH}/lib/sysutil_lib.sh"
 OUTDIR="${BASE_PATH}/data"
 
+echo "Starting database size capture @ ${CAPTUREID}"
+
+source ${LIB_PATH} && collect_db_size_info ${OUTDIR} ${CAPTUREID}
+
 echo "Starting system utilization capture @ ${CAPTUREID}"
 
-#for HOST in $(/opt/ibm/appliance/platform/xcat/scripts/xcat/display_nodes.py); do
-for HOST in node0101 ; do
+for HOST in $(/opt/ibm/appliance/platform/xcat/scripts/xcat/display_nodes.py); do
   echo "→ ${HOST}: capturing sysutil..."
   ssh "${HOST}" "source ${LIB_PATH} && run_sysutil_capture ${CAPTUREID} ${OUTDIR}" &
 done
 
 wait
 echo "✅ Completed all node captures for ${CAPTUREID}"
-
